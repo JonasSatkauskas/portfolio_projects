38e423c01a3a0dd2bb8ffecb66eac3e6edf76d26
JonasSatkauskas <satkusmedia@gmail.com>
2023-11-17 23:34:20 +0200

    stuff

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold bg-red-200 underline text-center">Yo</h1>
-  );
+  return <h1 className="text-3xl font-bold bg-red-200 text-center">Yo</h1>;
 }
 
 export default App;
